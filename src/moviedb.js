adf06049455f7a2a822b894a33537c440f38a595
reduansj <reduansenoun@gmail.com>
2022-09-06 12:59:42 +0200

    feature chalk display

--- src/moviedb.js
+++ src/moviedb.js
@@ -1,33 +1,9 @@
-// import { createRequire } from "node:module";
-// const require = createRequire(import.meta.url);
-
-// require("dotenv").config();
-
-// import { getPersons } from "./persons.js";
-
-// import { program } from "commander";
-
-// program
-//   .command("get-persons")
-//   .description("Make a network request to fetch the most popular persons")
-//   .requiredOption("-p, --popular", "Fetch the popular persons")
-//   .requiredOption(
-//     "--page <number>",
-//     "The page of persons data results to fetch"
-//   )
-//   .option("--details")
-//   .action((options) => {
-//     getPersons(options.page, process.env.API_KEY);
-//   });
-
-// program.parse();
-
 import { createRequire } from "node:module";
 const require = createRequire(import.meta.url);
 
 require("dotenv").config();
 
-import { getPersons } from "./persons.js";
+import { getPopularPersons } from "./persons.js";
 import { program } from "commander";
 
 program
@@ -39,7 +15,7 @@ program
     "The page of persons data results to fetch"
   )
   .action((options) => {
-    getPersons(options.page, process.env.API_KEY);
+    getPopularPersons(options.page, process.env.API_KEY);
   });
 
 program.parse();
--- src/persons.js
+++ src/persons.js
@@ -1,22 +1,30 @@
 import { get } from "node:https";
+import chalk from "chalk";
+
 import { readFile, writeFile } from "fs";
+
+const log = console.log;
 
 import ora from "ora";
 const spinner = ora("Loading unicorns");
 
-export function getPersons(page, apiKey) {
+export function getPopularPersons(page, apiKey) {
   spinner.start();
   get(
     `https://api.themoviedb.org/3/person/popular?api_key=${apiKey}&language=en-US&page=${page}`,
     (res) => {
-      console.log("statusCode:", res.statusCode);
-      console.log("headers:", res.headers);
+      let fetchedData = "";
+      res
+        .on("data", (d) => {
+          fetchedData += d;
 
-      res.on("data", (d) => {
-        process.stdout.write(d);
-        spinner.succeed();
-        spinner.stop();
-      });
+          // process.stdout.write(d);
+          spinner.succeed();
+        })
+        .on("end", () => {
+          setChalkColors(JSON.parse(fetchedData));
+          spinner.stop();
+        });
     }
   ).on("error", (e) => {
     console.error(e);
@@ -29,6 +37,36 @@ export function getPersons(page, apiKey) {
   }, 1000);
 }
 
-export function getDetails() {
-  console.log("details");
-}
+const setChalkColors = (popularPersonsData) => {
+  popularPersonsData.page < popularPersonsData.total_pages &&
+    log(
+      chalk.white(
+        `----------------------------------------------------- \n Page: ${popularPersonsData.page} of ${popularPersonsData.total_pages}`
+      )
+    );
+
+  getPersonData(popularPersonsData.results);
+};
+
+const getPersonData = (personData) => {
+  personData.map((person) => {
+    log(
+      chalk.white(
+        `----------------------------------------------------- \n`,
+        `Person: \n`,
+        `ID: ${person.id} \n`
+      ) +
+        chalk.blue.bold(`Name: ${person.name} \n`) +
+        (person.known_for_department === "Acting"
+          ? chalk.magenta(`Department: ${person.known_for_department}"  \n`)
+          : "")
+    );
+    getPersonMovie(person.known_for);
+  });
+};
+
+const getPersonMovie = (movies) => {
+  movies.map((movie) => {
+    movie.title !== undefined && log(movie.title);
+  });
+};
