91663d3c502bec9913ca0fab5860c2e2077f791e
reduansj <reduansenoun@gmail.com>
2022-09-06 11:14:19 +0200

    feature (change to ES5 with import & implement ora)

--- src/moviedb.js
+++ src/moviedb.js
@@ -1,7 +1,34 @@
-require("dotenv").config();
-const persons = require("./persons");
+// import { createRequire } from "node:module";
+// const require = createRequire(import.meta.url);
 
-const { program } = require("commander");
+// require("dotenv").config();
+
+// import { getPersons } from "./persons.js";
+
+// import { program } from "commander";
+
+// program
+//   .command("get-persons")
+//   .description("Make a network request to fetch the most popular persons")
+//   .requiredOption("-p, --popular", "Fetch the popular persons")
+//   .requiredOption(
+//     "--page <number>",
+//     "The page of persons data results to fetch"
+//   )
+//   .option("--details")
+//   .action((options) => {
+//     getPersons(options.page, process.env.API_KEY);
+//   });
+
+// program.parse();
+
+import { createRequire } from "node:module";
+const require = createRequire(import.meta.url);
+
+require("dotenv").config();
+
+import { getPersons } from "./persons.js";
+import { program } from "commander";
 
 program
   .command("get-persons")
@@ -12,7 +39,7 @@ program
     "The page of persons data results to fetch"
   )
   .action((options) => {
-    persons.getPersons(options.page, process.env.API_KEY);
+    getPersons(options.page, process.env.API_KEY);
   });
 
 program.parse();
--- src/persons.js
+++ src/persons.js
@@ -1,19 +1,34 @@
-const https = require("node:https");
+import { get } from "node:https";
+import { readFile, writeFile } from "fs";
 
-module.exports.getPersons = (page, apiKey) => {
-  https
-    .get(
-      `https://api.themoviedb.org/3/person/popular?api_key=${apiKey}&language=en-US&page=${page}`,
-      (res) => {
-        console.log("statusCode:", res.statusCode);
-        console.log("headers:", res.headers);
+import ora from "ora";
+const spinner = ora("Loading unicorns");
 
-        res.on("data", (d) => {
-          process.stdout.write(d);
-        });
-      }
-    )
-    .on("error", (e) => {
-      console.error(e);
-    });
-};
+export function getPersons(page, apiKey) {
+  spinner.start();
+  get(
+    `https://api.themoviedb.org/3/person/popular?api_key=${apiKey}&language=en-US&page=${page}`,
+    (res) => {
+      console.log("statusCode:", res.statusCode);
+      console.log("headers:", res.headers);
+
+      res.on("data", (d) => {
+        process.stdout.write(d);
+        spinner.succeed();
+        spinner.stop();
+      });
+    }
+  ).on("error", (e) => {
+    console.error(e);
+    spinner.fail("error on fetching");
+  });
+
+  setTimeout(() => {
+    spinner.color = "yellow";
+    spinner.text = "Loading rainbows";
+  }, 1000);
+}
+
+export function getDetails() {
+  console.log("details");
+}
