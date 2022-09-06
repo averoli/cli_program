e7d65c273ef58a05884249a643c459f6cca1bf04
MiguelD-PC <mikdominguez@gmail.com>
2022-09-06 10:24:42 +0100

    changes

--- src/moviedb.js
+++ src/moviedb.js
@@ -1,18 +1,42 @@
-require("dotenv").config();
-const persons = require("./persons");
+import { createRequire } from 'node:module';
+const require = createRequire(import.meta.url);
 
-const { program } = require("commander");
+require("dotenv").config();
+
+import ora from "ora";
+import { getPersons } from "./persons.js";
+import { program } from "commander";
+
+const spinner = ora('Loading unicorns');
 
 program
   .command("get-persons")
   .description("Make a network request to fetch the most popular persons")
   .requiredOption("-p, --popular", "Fetch the popular persons")
-  .requiredOption(
-    "--page <number>",
-    "The page of persons data results to fetch"
-  )
+  .requiredOption("--page <number>","The page of persons data results to fetch")
   .action((options) => {
-    persons.getPersons(options.page, process.env.API_KEY);
+    spinner.start();
+    setTimeout(() => {
+      getPersons(options.page, process.env.API_KEY);
+      spinner.stop();
+    }, 1000);
+  });
+
+  program
+  .command("get-person")
+  .description("Make a network request to fetch the data of a single person")
+  .requiredOption("-i, --id <number>", "The id of the person")
+  .action((options) => {
+    spinner.start();
+    setTimeout(() => {
+      getPersons(options.number, process.env.API_KEY);
+      spinner.stop();
+    }, 1000);
   });
 
 program.parse();
+
+setTimeout(() => {
+	spinner.color = 'yellow';
+	spinner.text = 'Loading rainbows';
+}, 1000);
--- src/persons.js
+++ src/persons.js
@@ -1,19 +1,38 @@
-const https = require("node:https");
+import https from "node:https";
 
-module.exports.getPersons = (page, apiKey) => {
-  https
-    .get(
-      `https://api.themoviedb.org/3/person/popular?api_key=${apiKey}&language=en-US&page=${page}`,
-      (res) => {
-        console.log("statusCode:", res.statusCode);
-        console.log("headers:", res.headers);
+export function getPersons(page, apiKey) {
 
-        res.on("data", (d) => {
-          process.stdout.write(d);
-        });
-      }
-    )
+  https.get(`https://api.themoviedb.org/3/person/popular?api_key=${apiKey}&language=en-US&page=${page}`, (res) => {
+      console.log("statusCode:", res.statusCode);
+      console.log("headers:", res.headers);
+
+      res.on("data", (d) => {
+        console.log(d);
+        process.stdout.write(d);
+      });
+
+    })
     .on("error", (e) => {
       console.error(e);
     });
-};
+
+  // const options = {
+  //   hostname: `https://api.themoviedb.org/3/person/popular?api_key=${apiKey}&page=${page}`,
+  //   port: 443,
+  //   path: '/todos',
+  //   method: 'GET'
+  // };
+
+  // const req = https.request(options, (res) => {999999999999999999999999999999999999999
+
+  //   res.on('data', (d) => {
+  //     console.log();
+  //     process.stdout.write(d);
+  //   });
+  // });
+
+  // req.on('error', (e) => {
+  //   console.error(e);
+  // });
+  // req.end();
+}
